8a4e4d8a745136486e672f7336e6a2e063cad896
Samok Sinha <samok.sinha@gmail.com>
2019-10-04 17:08:59 +0530

    Create JsonReader.java

---
+++ JsonReader.java
@@ -0,0 +1,82 @@
+package com.sam.jsonReader;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sam.exception.WebCrawlerException;
+import com.sam.model.WebPage;
+import com.sam.model.WebPageList;
+
+public final class JsonReader {
+
+	private static JsonReader jsonReader;
+	private ObjectMapper objectMapper;
+	private String jsonFilePath;
+	
+	private JsonReader(String jsonFilePath) {
+		this.jsonFilePath = jsonFilePath;
+		this.objectMapper = new ObjectMapper();
+	}
+	
+	public static JsonReader getReaderInstance(String jsonFilePath) {
+		
+		if(jsonReader == null) {
+			synchronized (JsonReader.class) {
+				if(jsonReader == null) {
+					jsonReader = new JsonReader(jsonFilePath);
+				}
+			}
+		}
+		
+		return jsonReader;
+	}
+	
+	public WebPageList readJsonFromFile() 
+			throws WebCrawlerException {
+		
+		WebPageList webPageList = new WebPageList();
+		try {
+			if(this.jsonFilePath != null
+					&& this.jsonFilePath.length() > 0) {
+				webPageList = objectMapper.readValue(new File(jsonFilePath), WebPageList.class);
+			}
+			
+		} catch (Exception e) {
+			throw new WebCrawlerException(101, e.getMessage(), e);
+		}
+		
+		return webPageList;
+	}
+	
+	public Map<String, List<String>> convertListToMap(List<WebPage> pages) 
+			throws WebCrawlerException {
+		
+		Map<String, List<String>> webCrawlerMap = null;
+		try {
+			if(pages != null
+					&& pages.size() > 0) {
+				
+				webCrawlerMap = new ConcurrentHashMap<String, List<String>>();
+				for(WebPage webPage : pages) {
+					webCrawlerMap.put(webPage.getAddress(), webPage.getLinks());
+				}
+			}
+			
+		} catch (Exception e) {
+			throw new WebCrawlerException(102, e.getMessage(), e);
+		}
+		
+		return webCrawlerMap;
+	}
+
+	public ObjectMapper getObjectMapper() {
+		return objectMapper;
+	}
+	public void setObjectMapper(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+
+}
