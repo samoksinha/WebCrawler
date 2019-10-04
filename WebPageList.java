125cebb350be9fed245aa033a55ede68326575c1
Samok Sinha <samok.sinha@gmail.com>
2019-10-04 17:09:51 +0530

    Create WebPageList.java

---
+++ WebPageList.java
@@ -0,0 +1,28 @@
+package com.sam.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebPageList implements Serializable {
+
+	private static final long serialVersionUID = -5784881159898202643L;
+	
+	private List<WebPage> pages;
+	
+	public WebPageList() {
+		this.pages = new ArrayList<WebPage>();
+	}
+	
+	public WebPageList(List<WebPage> pages) {
+		this.pages = pages;
+	}
+
+	public List<WebPage> getPages() {
+		return pages;
+	}
+	public void setPages(List<WebPage> pages) {
+		this.pages = pages;
+	}
+	
+}
